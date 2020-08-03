98bf4f9bc13bcb0353038a33ac60638f05603a03
fengmingchuan <57157957+fengmingchuan@users.noreply.github.com>
2020-08-03 11:12:46 +0800

    Update Course.java

--- src/main/java/com/fmc/jpa/dao/Course.java
+++ src/main/java/com/fmc/jpa/dao/Course.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Component
 public interface Course   extends JpaRepository<EduCourse,String > , JpaSpecificationExecutor<EduCourse> {
+        //按名字查询
 @Query(value = "SELECT ec.title,ec.price,et.name FROM  edu_teacher et INNER JOIN edu_course ec WHERE" +
         " et.id=ec.teacher_id AND ec.title LIKE '%HTML%'",nativeQuery = true)
     List<Object[]>  selectByNameAndTitle(  String title);
